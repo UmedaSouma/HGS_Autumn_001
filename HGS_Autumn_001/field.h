2f81f363545c0c059bb7ca358a81465576f53e6d
KasaiKeisuke <231020@st.yoshida-g.ac.jp>
2024-09-30 13:56:49 +0900

    cppとhの追加

--- HGS_Autumn_001/field.h
+++ HGS_Autumn_001/field.h
@@ -1,28 +1,6 @@
 //===========================================================================================================================================================
 // 
-// field.cppのヘッダー [field.h]
+// field.cppのヘッダー
 // Author : Kasai Keisuke
 // 
 //===========================================================================================================================================================
-
-#ifndef _FIELD_H_
-#define _FIELD_H_
-
-// include
-#include "main.h"
-#include "object3D.h"
-
-// 床クラスの定義
-class CField : public CObject3D
-{
-	CField();				// コンストラクタ
-	~CField()override;		// デストラクタ
-	HRESULT Init()override;	// 初期設定
-	void Uninit()override;	// 終了
-	void Update()override;	// 更新
-	void Draw()override;	// 描画
-
-	static CField* Create(D3DXVECTOR3 pos);   // 生成処理
-
-};
-#endif
